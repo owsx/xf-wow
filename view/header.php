0c4cf71872a76c0fb2f6f12ee185e906162a9dcd
owsx <owsx@163.com>
2013-10-17 15:04:04 +0800

    Create header.php

---
+++ view/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=<?=C('charset')?>" />
+	<title><?=web_title()?></title>
+	<link rel="stylesheet" type="text/css" href="<?=_U_R?>theme/<?=C('theme')?>/main.css" />
+	<script type="text/javascript" src="<?=_U_R?>js/jquery/jquery.js"></script>
+	<script type="text/javascript" src="<?=_U_R?>js/jquery/jquery-ui.js"></script>
+	<script type="text/javascript" src="<?=_U_R?>js/jquery/jquery.ui.datepicker-zh-CN.js" charset="utf-8"></script>
+	<link type="text/css" rel="stylesheet" href="<?=_U_R?>js/jquery/jquery-ui.css" />
+	<script type="text/javascript" src="<?=_U_R?>js/common.js" charset="utf-8"></script>
+</head>
+<body>
+<center>
+<div id="body">
+
+<div id="header">
+<?=web_header()?>
+</div>
+
+<div id="index">
+
+<div id="mod">
+<?=web_mod()?>
+</div>
+
+<!-- start of main -->
+<div id="main">
+
+
