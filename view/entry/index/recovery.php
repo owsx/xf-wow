b7a0c6b4530bafa8e453183310d06e5f3a1bbff5
owsx <owsx@163.com>
2013-10-17 14:59:15 +0800

    Create recovery.php

---
+++ view/entry/index/recovery.php
@@ -0,0 +1,65 @@
+<?=$this->load('/header')?>
+
+<script src="<?=_U_R?>js/sha1.js"></script>
+<script>
+function check_form() {
+	var f = document.form;
+	if ( !f.uid.value ) {
+		alert("<?=L('err.userid_blank')?>");
+		f.uid.focus();
+		return false;
+	}
+	if ( !f.upass1.value || !f.upass2.value ) {
+		alert("<?=L('err.userpass_blank')?>");
+		return false;
+	}
+	if ( f.upass1.value != f.upass2.value ) {
+		alert("<?=L('err.diff_pass')?>");
+		return false;
+	}
+	if ( !f.email.value ) {
+		alert("<?=L('err.email_blank')?>");
+		f.email.focus();
+		return false;
+	}
+	f.upass.value = hex_sha1(f.uid.value.toUpperCase()+":"+f.upass1.value.toUpperCase());
+	f.upass1.value = "";
+	f.upass2.value = "";
+	return true;
+}
+</script>
+
+<fieldset>
+<legend><?=L('recovery')?></legend>
+<form name="form" method="post" action="<?=U('do_recovery')?>" onsubmit="return check_form()">
+<input type="hidden" name="upass" maxlength="250" />
+<table>
+<tbody>
+<tr>
+	<th><?=L('userid')?>:</th>
+	<td><input type="text" name="uid" maxlength="32" required="required" placeholder="请输入账号" /></td>
+</tr>
+<tr>
+	<th><?=L('email')?>:</th>
+	<td><input type="text" name="email" maxlength="64" required="required" placeholder="请输入电子邮箱地址" /></td>
+</tr>
+<tr>
+	<th><?=L('newpass')?>:</th>
+	<td><input type="password" name="upass1" maxlength="32" required="required" placeholder="请设置新密码" /></td>
+</tr>
+<tr>
+	<th><?=L('repeatpass')?>:</th>
+	<td><input type="password" name="upass2" maxlength="32" required="required" placeholder="请重复输入上面的密码" /></td>
+</tr>
+</tbody>
+<tfoot>
+<tr>
+	<th></th>
+	<td><button type="submit"><?=L('recovery')?></button></td>
+</tr>
+</tfoot>
+</table>
+</form>
+</fieldset>
+
+<?=$this->load('/footer')?>
