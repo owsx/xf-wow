e60a3d84f8607f60e34f74cdb15347d93f835986
owsx <owsx@163.com>
2013-10-17 14:47:22 +0800

    Create wow.php

---
+++ config/wow.php
@@ -0,0 +1,308 @@
+<?php
+
+return array(
+
+
+'user.ulevel' => array(
+	0 => 'user',
+	1 => 'user',
+	2 => 'test',
+	3 => 'test',
+	4 => 'test',
+	5 => 'admin',
+	6 => 'test',
+	7 => 'test',
+	8 => 'test',
+	9 => 'test',
+),
+
+'web.menu' => array(
+	array('lyb','/index/lyb','留言板',1),
+),
+
+'item.quality' => array(
+	array('0','破损'),     // poor grey
+	array('1','普通'),     // common white
+	array('2','精良'),     // uncommon green
+	array('3','稀有'),     // rare blue
+	array('4','史诗'),     // epic purple
+	array('5','传说'),     // legendary orange
+	array('6','神造'),     // artifact red
+	array('7','传家宝'),     // bind to account gold
+),
+
+'item.class' => array(
+	array('0','消耗品'),     // consumable
+	array('1','容器'),     // container
+	array('2','武器'),     // weapon
+	array('3','宝石'),     // gem
+	array('4','盔甲'),     // armor
+	array('5','药剂'),     // reagent
+	array('6','发射物'),     // projectile
+	array('7','交易物'),     // trade_goods
+	//array('8','x'),     // generic
+	array('9','配方'),     // recipe
+	//array('10','x'),     // money (obsolete)
+	array('11','弹药'),     // quiver
+	array('12','任务'),     // quest
+	array('13','钥匙'),     // key
+	//array('14','永久的'),     // permanent (obsolete)
+	array('15','杂物'),     // miscellaneous
+	array('16','雕纹'),     // glyph
+),
+
+'item.subclass' => array(
+	array('0_0','消耗品'),     // consumable
+	array('0_1','瓶子'),     // potion
+	array('0_2','药剂'),     // elixir
+	array('0_3','药水'),     // flask
+	array('0_4','卷轴'),     // scroll
+	array('0_5','饮食'),     // food&drink
+	array('0_6','物品增强'),     // item enhancement
+	array('0_7','绷带'),     // bandage
+	array('0_8','其他'),     // other
+	array('1_0','背包'),     // bag
+	array('1_1','灵魂包'),     // soul bag
+	array('1_2','草药包'),     // herb bag
+	array('1_3','附魔包'),     // enchanting bag
+	array('1_4','工程包'),     // engineering bag
+	array('1_5','宝石包'),     // gem bag
+	array('1_6','矿石包'),     // mining bag
+	array('1_7','制皮包'),     // leatherworking bag
+	array('1_8','抄写包'),     // inscription bag
+	array('2_0','单手斧'),     // axe one handed
+	array('2_1','双手斧'),     // axe two handed
+	array('2_2','弓'),     // bow
+	array('2_3','火枪'),     // gun
+	array('2_4','单手锤'),     // mace one handed
+	array('2_5','双手锤'),     // mace two handed
+	array('2_6','长矛'),     // polearm
+	array('2_7','单手剑'),     // sword
+	array('2_8','双手剑'),     // sword
+	//array('2_9','武器'),     // obsolete
+	array('2_10','法杖'),     // staff
+	//array('2_11','x'),     // exotic
+	//array('2_12','x'),     // exotic
+	array('2_13','拳套'),     // fist weapon
+	array('2_14','其他'),     // miscellaneous (blacksmith hammer,mining pick)
+	array('2_15','匕首'),     // dagger
+	array('2_16','投掷'),     // thrown
+	//array('2_17','长枪'),     // spear
+	array('2_18','弩'),     // crossbow
+	array('2_19','魔杖'),     // wand
+	array('2_20','鱼竿'),     // fishing pole
+	array('3_0','红色宝石'),     // red
+	array('3_1','蓝色宝石'),     // blue
+	array('3_2','黄色宝石'),     // yellow
+	array('3_3','紫色宝石'),     // purple
+	array('3_4','绿色宝石'),     // green
+	array('3_5','橙色宝石'),     // orange
+	array('3_6','多彩宝石'),     // meta
+	array('3_7','简单宝石'),     // simple
+	array('3_8','棱镜宝石'),     // prismatic
+	array('4_0','其他'),     // miscellaneous
+	array('4_1','布甲'),     // cloth
+	array('4_2','皮甲'),     // leather
+	array('4_3','链甲'),     // mail
+	array('4_4','铠甲'),     // plate
+	//array('4_5','小盾'),     // buckler (obsolete)
+	array('4_6','盾牌'),     // shield
+	array('4_7','图纹'),     // libram
+	array('4_8','圣物'),     // idol
+	array('4_9','图腾'),     // totem
+	array('4_10','印记'),     // sigil
+	array('5_0','药剂'),     // reagent
+	//array('6_0','魔杖'),     // wand (obsolete)
+	//array('6_1','闪电'),     // bolt (obsolete)
+	array('6_2','弓箭'),     // arrow
+	array('6_3','子弹'),     // bullet
+	//array('6_4','投掷物'),     // thrown (obsolete)
+	//array('7_0','交易物'),     // trade goods
+	array('7_1','零件'),     // parts
+	array('7_2','爆炸物'),     // explosives
+	array('7_3','装置'),     // devices
+	array('7_4','工艺品'),     // jewelcrafting
+	array('7_5','布'),     // cloth
+	array('7_6','皮'),     // leather
+	array('7_7','石头'),     // metal&stone
+	array('7_8','肉'),     // meat
+	array('7_9','草药'),     // herb
+	array('7_10','元素'),     // elemental
+	array('7_11','其他'),     // other
+	array('7_12','附魔'),     // enchanting
+	array('7_13','原料'),     // materials
+	array('7_14','盔甲增强'),     // armor enchantment
+	array('7_15','武器增强'),     // weapon enchantment
+	//array('8_0','x'),     // generic
+	array('9_0','书'),     // book
+	array('9_1','制皮图样'),     // leatherworking Pattern
+	array('9_2','裁缝图样'),     // tailoring Pattern
+	array('9_3','工程设计图'),     // engineering Schematic
+	array('9_4','锻造设计图'),     // blacksmithing Plans
+	array('9_5','烹饪配方'),     // cooking Recipe
+	array('9_6','炼金配方'),     // alchemy Recipe
+	array('9_7','急救手册'),     // first aid Manual/Formula
+	array('9_8','附魔公式'),     // enchanting Recipe
+	array('9_9','钓鱼手册'),     // fishing
+	array('9_10','珠宝设计图'),     // jewelcrafting Design
+	//array('10_0','x'),     // money
+	//array('11_0','x'),     // quiver
+	//array('11_1','x'),     // quiver
+	array('11_2','弓箭'),     // quiver
+	array('11_3','子弹'),     // ammo pouch
+	array('12_0','任务'),     // quest
+	array('13_0','钥匙'),     // key
+	array('13_1','开锁工具'),     // lockpick
+	//array('14_0','永久的'),     // permanent
+	array('15_0','垃圾'),     // junk
+	array('15_1','试剂'),     // reagent
+	array('15_2','小宠物'),     // pet
+	array('15_3','节日'),     // holiday
+	array('15_4','其他'),     // other
+	array('15_5','坐骑'),     // mount
+	array('16_1','战士'),     // warrior
+	array('16_2','圣骑士'),     // paladin
+	array('16_3','猎人'),     // hunter
+	array('16_4','盗贼'),     // rogue
+	array('16_5','牧师'),     // priest
+	array('16_6','死亡骑士'),     // death knight
+	array('16_7','萨满'),     // shaman
+	array('16_8','魔法师'),     // mage
+	array('16_9','术士'),     // warlock
+	array('16_11','德鲁伊'),     // druid
+),
+
+'item.itype' => array(
+	array('0','-'),     // 
+	array('1','头部'),     // head
+	array('2','颈部'),     // neck
+	array('3','肩部'),     // shoulder
+	array('4','衬衣'),     // shirt
+	array('5','胸部'),     // chest
+	array('6','腰带'),     // waist
+	array('7','腿部'),     // legs
+	array('8','脚部'),     // feet
+	array('9','手腕'),     // wrists
+	array('10','手套'),     // gloves
+	array('11','戒指'),     // finger
+	array('12','饰品'),     // trinket
+	array('13','单手'),     // weapon
+	array('14','副手'),     // shield
+	array('15','远程'),     // ranged (bow)
+	array('16','背部'),     // back
+	array('17','双手'),     // two_hand
+	array('18','背包'),     // bag
+	array('19','战袍'),     // tabard
+	array('20','长袍'),     // robe
+	array('21','主手'),     // main_hand
+	array('22','副手'),     // off hand
+	array('23','手持'),     // holdable tome
+	array('24','子弹'),     // ammo
+	array('25','投掷物'),     // thrown
+	array('26','射击物'),     // ranged right (wands,guns)
+	array('27','弓箭'),     // quiver
+	array('28','纪念物'),     // relic
+	
+),
+
+'item.bonding' => array(
+	array('0','不绑定'),     // no bounds
+	array('1','拾取绑定'),     // binds when picked up
+	array('2','装备绑定'),     // binds when equipped
+	array('3','使用绑定'),     // binds when used
+	array('4','任务物品'),     // quest item
+	//array('5','任务物品2'),     // quest item
+),
+
+'item.stat_type' => array(
+	array('0','魔法值'),     // mana
+	array('1','生命值'),     // health
+	array('3','敏捷'),     // agility
+	array('4','力量'),     // strength
+	array('5','智力'),     // intellect
+	array('6','精神'),     // spirit
+	array('7','体力'),     // stamina
+	array('12','防御'),     // DEFENCE_SKILL_RATING
+	array('13','躲闪'),     // DODGE_RATING
+	array('14','招架'),     // PARRY_RATING
+	array('15','格挡'),     // SHIELD_BLOCK_RATING
+	array('16','近战命中'),     // HIT_MELEE_RATING
+	array('17','远程命中'),     // HIT_RANGED_RATING
+	array('18','法术命中'),     // HIT_SPELL_RATING
+	array('19','近战暴击'),     // CRIT_MELEE_RATING
+	array('20','远程暴击'),     // CRIT_RANGED_RATING
+	array('21','法术暴击'),     // CRIT_SPELL_RATING
+	array('22','近战命中躲闪'),     // HIT_TAKEN_MELEE_RATING
+	array('23','远程命中躲闪'),     // HIT_TAKEN_RANGED_RATING
+	array('24','法术命中躲闪'),     // HIT_TAKEN_SPELL_RATING
+	array('25','近战暴击躲闪'),     // CRIT_TAKEN_MELEE_RATING
+	array('26','远程暴击躲闪'),     // CRIT_TAKEN_RANGED_RATING
+	array('27','法术暴击躲闪'),     // CRIT_TAKEN_SPELL_RATING
+	array('28','近战急速'),     // HASTE_MELEE_RATING
+	array('29','远程急速'),     // HASTE_RANGED_RATING
+	array('30','法术急速'),     // HASTE_SPELL_RATING
+	array('31','命中'),     // HIT_RATING
+	array('32','暴击'),     // CRIT_RATING
+	array('33','命中躲闪'),     // HIT_TAKEN_RATING
+	array('34','暴击躲闪'),     // CRIT_TAKEN_RATING
+	array('35','韧性'),     // RESILIENCE_RATING
+	array('36','急速'),     // HASTE_RATING
+	array('37','急速'),     // EXPERTISE_RATING
+	array('38','攻击强度'),     // ATTACK_POWER
+	array('39','远程攻击强度'),     // RANGED_ATTACK_POWER
+	//array('40','对野兽伤害'),     // FERAL_ATTACK_POWER
+	array('41','治疗能量'),     // SPELL_HEALING_DONE
+	array('42','法术能量'),     // SPELL_DAMAGE_DONE
+	array('43','魔法恢复'),     // MANA_REGENERATION
+	array('44','防御穿透'),     // ARMOR_PENETRATION_RATING
+	array('45','法术强度'),     // SPELL_POWER
+	array('46','生命恢复'),     // HEALTH_REGEN
+	array('47','法术穿透'),     // SPELL_PENETRATION
+	array('48','格挡值'),     // BLOCK_VALUE
+),
+
+'item.material' => array(
+	array('-1','消耗品'),     // consumables
+	array('0','默认'),     // not defined
+	array('1','金属'),     // metal
+	array('2','木头'),     // wood
+	array('3','液体'),     // liquid
+	array('4','珠宝'),     // jewelry
+	array('5','链甲'),     // chain
+	array('6','铠甲'),     // plate
+	array('7','布类'),     // cloth
+	array('8','皮革'),     // leather
+),
+
+'damage.type' => array(
+	array('0','物理'),     // physical
+	array('1','神圣'),     // holy
+	array('2','火焰'),     // fire
+	array('3','自然'),     // nature
+	array('4','冰霜'),     // frost
+	array('5','暗影'),     // shadow
+	array('6','奥术'),     // arcane
+),
+
+'item.spelltrigger' => array(
+	array('0','使用触发'),     // use
+	array('1','装备触发'),     // on equip
+	array('2','击中触发'),     // chance on hit
+	array('3','灵魂石'),     // soulstone
+	array('4','使用无延迟'),     // use with no delay
+	array('5','学习法术'),     // learn spell id
+),
+
+'reputation.rank' => array(
+	array('0','仇恨'),     // hated
+	array('1','敌对'),     // hostile
+	array('2','不友好'),     // unfriendly
+	array('3','中立'),     // neutral
+	array('4','友好'),     // friendly
+	array('5','尊敬'),     // honored
+	array('6','崇敬'),     // revered
+	array('7','崇拜'),     // exalted
+),
+
+
+);
